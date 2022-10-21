1f4b121d162a1d33bc3c9772da9acd0e5d36345a
antony rajan <53305623+antonyrajancloud1@users.noreply.github.com>
2022-10-21 08:38:15 +0530

    Update algoScalpingBankNifty.py

--- algoScalpingBankNifty.py
+++ algoScalpingBankNifty.py
@@ -108,8 +108,6 @@ def placePutOption(message):
         exitOrder(message)
         #checkIfOrderExists()
         optionToBuy = getTradingSymbol() + str(getCurrentAtm()+500) + "PE"
-        global currentPremiumPlaced
-        currentPremiumPlaced = optionToBuy
         order_id = kite.place_order(tradingsymbol=optionToBuy, variety=kite.VARIETY_REGULAR, exchange=kite.EXCHANGE_NFO,
                                     transaction_type=kite.TRANSACTION_TYPE_BUY, quantity=qty,
                                     order_type=kite.ORDER_TYPE_MARKET, product=kite.PRODUCT_MIS)
